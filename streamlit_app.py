f65a82e82ac0203e68151ce40707c3bc940da999
Lucas Pessoa <158770857+lucasspessoa@users.noreply.github.com>
2024-10-30 15:18:31 -0300

    etapa-30.10

--- streamlit_app.py
+++ streamlit_app.py
@@ -72,14 +72,32 @@ elif page == "Projetos Realizados":
     st.header("Projetos")
     st.write("Aqui estão alguns dos projetos em que trabalhei:")
 
-    # Projeto 1: Netflix 1997 Dashboard
-    if st.button("Netflix 1997 Dashboard"):
-        st.write("Análise de Lucro e Performance, com dados fictícios, da empresa de streaming Netflix, criada em 1997, baseada no aluguel de DVDs de filmes de sucesso do ano de 1998. Como objetivo, o projeto visa demonstrar a importância da Análise de Dados desde muito antes de surgir ferramentas poderosas como o Power BI.")
+    # Projeto 1: Dashboard Netflix 1997
+    if st.button("Dashboard Netflix 1997"):
+        st.write("Com dados fictícios, analisei Lucro e Produto da empresa de streaming Netflix, criada em 1997, baseada no aluguel de DVDs de filmes de sucesso do ano de 1998 (nostálgico?).")
+        st.markdown("""
+        Além da criatividade em demonstrar a importância da Análise de Dados muito antes de existir ferramentas como Power BI, utilizei o dashboard para exibir gráficos-chave para o tipo de análise que se propõe:
+        - **Gráfico de Linha**: excelente para acompanhar tendências ao longo do tempo;
+        - **Gráfico de Barras Agrupadas**: demonstra a relação de magnitude entre duas variáveis;
+        - **Gráfico de Pizza**: embora exista limitação de elementos (recomendável não mais que 5), é útil para mostrar a composição de um todo;
+        - **Mapa**: perfeito para visualizações geográficas.
+
+        Plus: no Gráfico de Pizza, ainda foi inclusa a "Tooltip", ou Dica de Ferramenta, para mostrar a composição dos tipos de devoluções em relação aos filmes listados. Para visualizá-la, passe o mouse por cima de cada fatia.
+        """)
         st.write("[Acesse o dashboard publicado no Power BI](https://app.powerbi.com/view?r=eyJrIjoiMTcxOTVkZjMtNTZmMS00OWVlLWI4ZGItMWFjZjJhZWY0ZjI2IiwidCI6ImI1OTFhZTU0LTMzYzItNDU4OS1iZTY2LTkwMjFhNDE5NmM3YyJ9&disablecdnExpiration=1728858578)")
 
     # Projeto 2: Dashboard Indicadores
     if st.button("Dashboard Indicadores"):
-        st.write("Neste dashboard criado, pude me aprofundar no trabalho de Performance, voltado a analisar indicadores de uma Promotora de crédito, com DADOS FICTÍCIOS. Este exercício me fez se aprofundar em gráficos nativos do Power BI que são úteis para essa área tão fundamental para o desempenho de uma empresa: KPI, muito popular para análise relativa; Velocímetro, voltado a metas; Treemap, que permite visualizar hierarquia; tabela simples, sempre bem-vinda.")
+        st.write("Neste dashboard, pude me aprofundar no trabalho de Performance, voltado a analisar indicadores de uma Promotora de crédito consignado, com DADOS FICTÍCIOS.") 
+        st.markdown("""
+        Este exercício me fez se aprofundar em gráficos nativos do Power BI que são úteis para essa área tão fundamental voltada ao desempenho de uma empresa:
+        - **KPI**: muito popular para análise relativa. No dashboard, exibe a produção do último mês em relação à meta mensal da empresa;
+        - **Velocímetro**: voltado a metas. No dashboard, exibe a produção total realizada no período e a meta estipulada;
+        - **Treemap**: permite visualizar hierarquia. No dashboard, mostra a distribuição da produção de acordo com o convênio do negócio;
+        - **Tabela Simples**: sempre bem-vinda. No dashboard, mostra quem atingiu a meta específica (selo verdinho) de R$ 250 mil no convênio Privado.
+
+        Embora nada extravagante, também foquei em ferramentas de estilização. Uso de símbolos, paleta de cores e a visualização "clean" são marcas que particulamente me encantam na parte de design.
+        """)
         st.write("[Acesse o dashboard publicado no Power BI](https://app.powerbi.com/view?r=eyJrIjoiMjNlNzQzZWEtYzVmMi00NTJhLWIxZDctZWQ2OWVmMjRkYmZhIiwidCI6ImI1OTFhZTU0LTMzYzItNDU4OS1iZTY2LTkwMjFhNDE5NmM3YyJ9)")
 
         # Projeto 3: Repositório GitHub
